d5749f7183d5994c68e1fcdb169b86dd61095a5b
Aleksandr Shatokhin <aleks_shatoh@mail.ru>
2020-09-12 13:45:30 +0300

    Updates

---
+++ precache-manifest.9197a3c5ad2a9bb815975183f93671c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daae113c4850b3d084e1ec72b78543da",
+    "url": "/To-do-react/index.html"
+  },
+  {
+    "revision": "4a031787af6dfe641464",
+    "url": "/To-do-react/static/css/main.173cfa3b.chunk.css"
+  },
+  {
+    "revision": "899712c8debbce5cb876",
+    "url": "/To-do-react/static/js/2.a277081d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-react/static/js/2.a277081d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a031787af6dfe641464",
+    "url": "/To-do-react/static/js/main.a5127020.chunk.js"
+  },
+  {
+    "revision": "551b32621aa85d0a0a0f",
+    "url": "/To-do-react/static/js/runtime-main.e70f7ea1.js"
+  }
+]);
